1c3cdad372d192a9be21541e802ff4c0f04a443f
Passawut <passawut.prinyagul@gmail.com>
2025-04-16 17:16:55 +0700

    fix: add task_id in tasks

--- app/tasks.py
+++ app/tasks.py
@@ -4,8 +4,9 @@ from app.feature_extraction import process_files
 
 @celery_app.task(bind=True)
 def process_urls_task(self, whitelist_path: str, blacklist_path: str) -> dict:
+    task_id = self.request.id
     try:
-        csv_path = process_files(whitelist_path, blacklist_path)
+        csv_path = process_files(whitelist_path, blacklist_path, task_id)
         return {"status": "SUCCESS", "csv_path": csv_path}
     except Exception as e:
         return {"status": "FAILURE", "error": str(e)}
